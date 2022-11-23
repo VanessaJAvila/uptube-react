971713279b29b1b0727bdde743791fa3f054d26a
Vanessa <vanessa.3bl@gmail.com>
2022-11-23 22:21:43 +0000

    Header

--- client/src/Layout/Header.js
+++ client/src/Layout/Header.js
@@ -1,12 +1,30 @@
 import "./Header.scss";
-import logo from "./logo.png";
+import logo from "./logo.svg";
+import {useState} from "react";
+import axios from "axios";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import{faCircleUser} from "@fortawesome/free-regular-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function Header() {
+
+    const [filter, setFilter] = useState("");
+
     return <div className={"Header"}>
         <div className={"logo"}>
         <img src={logo} alt ="logo UpTube"/>
-        <span>UpTube</span>
+        </div>
+            <div className={"searching"}>
+                <FontAwesomeIcon className={"s-icon"} icon={faMagnifyingGlass}/>
+                <input className={"search"}
+                       type="text"
+                       placeholder={"Pesquisar"}
+                       onChange={e => setFilter(e.target.value)}/>
             </div>
+        <div className={"login"}>
+            <input className={"button"} type="button" value="Iniciar Sessão" onClick="msg()"/>
+            <FontAwesomeIcon className={"l-icon"} icon={faCircleUser}/>
+        </div>
     </div>
 }
 
