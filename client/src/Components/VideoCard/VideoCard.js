fc4f3e7d79f09dea21ffe7ece17c8271f5e52a99
Rita <ritaisabelmq@gmail.com>
2022-12-05 17:59:12 +0000

    image - video card

--- client/src/Components/VideoCard/VideoCard.js
+++ client/src/Components/VideoCard/VideoCard.js
@@ -9,7 +9,8 @@ function VideoCard({title, date, duration, thumbnail, likes, comments, views, us
             <div className="title">{title}</div>
             <div className="days-posted">há {getNumberOfDays(date, Date.now())} dias</div>
             <div className="length">{duration}</div>
-            <div className="thumbnail">thumbnail: {thumbnail}</div>
+            <div className="thumbnail">
+                <img src={`https://github.com/upskill-frontend-react-2022/uptube-grupo-5-react/${thumbnail}`}/></div>
             <div className="likes">{likes} likes</div>
             <div className="comments">{comments} comments</div>
             <div className="views">{views} visualizações</div>
